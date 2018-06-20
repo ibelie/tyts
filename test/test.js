fb047b30c299efc430cff11c62206df983cc7781
joungtao <joungtao@gmail.com>
2018-06-20 11:57:43 +0800

    refresh license

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 "use strict";
-// Copyright 2017-2018 ibelie, Chen Jie, Joungtao. All rights reserved.
+// Copyright 2017 - 2018 ibelie, Chen Jie, Joungtao. All rights reserved.
 // Use of this source code is governed by The MIT License
 // that can be found in the LICENSE file.
 Object.defineProperty(exports, "__esModule", { value: true });
--- tyts.js
+++ tyts.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2018 ibelie, Chen Jie, Joungtao. All rights reserved.
+// Copyright 2017 - 2018 ibelie, Chen Jie, Joungtao. All rights reserved.
 // Use of this source code is governed by The MIT License
 // that can be found in the LICENSE file.
 
